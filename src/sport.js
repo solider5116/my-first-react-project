3a516455cf492e98e42728e4edf5a279fa7226f8
solider5116 <solider1685@gmail.com>
2023-04-19 23:03:04 +0530

    image practice

---
+++ src/sport.js
@@ -0,0 +1,9 @@
+function Sport() {
+  return (
+    <div>
+      <img src="https://thumbs.dreamstime.com/b/sport-collage-boxing-soccer-american-football-basketball-baseball-ice-hockey-etc-multi-professional-tennis-l-bascketball-players-93401905.jpg"></img>
+      <img src="https://media.istockphoto.com/id/1367872098/photo/full-length-shot-of-a-handsome-young-male-athlete-running-on-an-outdoor-track.jpg?s=612x612&w=is&k=20&c=bmlMmFCq3nY1EcBsIZdnaJjTUoGRz76Z4EWyAifHmIw="></img>
+    </div>
+  );
+}
+export default Sport;
